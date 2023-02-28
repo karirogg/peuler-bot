6eb3371ad9b5dc6481e127f9fae5bfdb35a1e9dc
Kári Rögnvaldsson <kari@vr2t-dw392.rhi.hi.is>
2023-02-28 09:15:36 +0000

    FastAPI model ready to try in production

---
+++ test.py
@@ -0,0 +1,13 @@
+import base64
+with open("img/captcha_1.png", "rb") as image_file:
+    base64str = base64.b64encode(image_file.read()).decode("utf-8")
+
+import requests, json
+payload = json.dumps({
+  "base64str": base64str,
+  "threshold": 0.5
+})
+response = requests.put("http://127.0.0.1:8000/predict",data = payload)
+data_dict = response.json()
+
+print(data_dict)
