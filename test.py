42e9e61691b0674ac842375714b6b83cc41022f6
Kári Rögnvaldsson <kari@Kraptop.local>
2023-03-01 19:59:12 +0000

    Final version

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import base64
-with open("img/captcha_1.png", "rb") as image_file:
+
+path = input("Enter image name:")
+
+with open(f"{path}", "rb") as image_file:
     base64str = base64.b64encode(image_file.read()).decode("utf-8")
 
 import requests, json
@@ -7,7 +10,7 @@ payload = json.dumps({
   "base64str": base64str,
   "threshold": 0.5
 })
-response = requests.put("http://52.30.21.139/predict",data = payload)
+response = requests.put("http://54.217.150.250/predict",data = payload)
 data_dict = response.json()
 
 print(data_dict)
